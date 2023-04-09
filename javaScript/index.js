71f20dd8ce3965f4a46457e190bdd29e8d066a72
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-09 20:59:17 -0300

    Correção de bug

--- javaScript/index.js
+++ javaScript/index.js
@@ -17,7 +17,7 @@ function sorteiaNumero() {
 	verificaAcertoOuErro(numeroSorteado);
 }
 
-function verificaAcertoOuErro(numeroSorteado) {
+function verificaAcertoOuErro(numeroSorteado, numeroDigitado) {
 	if (numeroDigitado == numeroSorteado) {
 		resposta.textContent = "Parabéns você acertou!!!";
 	} else {
@@ -32,7 +32,11 @@ function verificaValorDigitado(numeroDigitado) {
 
 	if (isNaN(numeroDigitado)) {
 		mensagem.textContent = "Digite somente números!";
+	} else if (numeroDigitado.length == 0) {
+		mensagem.textContent = "Digite algum número!";
+	} else if (numeroDigitado < 0 || numeroDigitado > 10) {
+		mensagem.textContent = "Digite somente números de 0 a 10!";
 	} else {
-		mensagem.textContent = "Ok!";
+		mensagem.textContent = "Número válido.";
 	}
 }
