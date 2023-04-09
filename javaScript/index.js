d8e6c98940b5217efb29558113a63ed013c7b782
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-09 19:40:22 -0300

    Função para sortear número

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,5 +1,13 @@
 function verificaAcertoOuErro() {
-	var campoNumero = document.getElementById("entrada");
+	
+}
 
-	console.log(campoNumero);
+function sorteiaNumero() {
+	//Armazenando um número aleatório de 0 a 10
+	var numero = Math.random() * 10;
+
+	//Arredondando o número
+	numero = Math.round(numero);
+
+	console.log(numero);
 }
