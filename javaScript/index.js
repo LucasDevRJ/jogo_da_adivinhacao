b7b92ee5be844b908249820722ec07b2db518f25
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-09 19:47:01 -0300

    Verifica se o usuário acertou ou errou o número

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,13 +1,22 @@
-function verificaAcertoOuErro() {
-	
-}
-
 function sorteiaNumero() {
 	//Armazenando um número aleatório de 0 a 10
-	var numero = Math.random() * 10;
+	var numeroSorteado = Math.random() * 10;
 
 	//Arredondando o número
-	numero = Math.round(numero);
+	numeroSorteado = Math.round(numeroSorteado);
 
-	console.log(numero);
+	verificaAcertoOuErro(numeroSorteado);
+}
+
+function verificaAcertoOuErro(numeroSorteado) {
+	var campoNumero = document.getElementById("entrada");
+	var resposta = document.getElementById("resposta");
+
+	var numeroDigitado = campoNumero.value;
+
+	if (numeroDigitado == numeroSorteado) {
+		resposta.textContent = "Parabéns você acertou!!!";
+	} else {
+		resposta.textContent = "Errou!! O número sorteado é " + numeroSorteado;
+	}
 }
