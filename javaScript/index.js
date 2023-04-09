d832e005f9209984165b3ff5a76686622ceb0cce
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-09 20:11:31 -0300

    Verifica se o número é válido ou não

--- javaScript/index.js
+++ javaScript/index.js
@@ -11,12 +11,30 @@ function sorteiaNumero() {
 function verificaAcertoOuErro(numeroSorteado) {
 	var campoNumero = document.getElementById("entrada");
 	var resposta = document.getElementById("resposta");
+	var erro = document.getElementById("erro");
 
 	var numeroDigitado = campoNumero.value;
+
+	if (numeroDigitado < 0 || numeroDigitado > 10) {
+		erro.textContent = "Digite somente números entre 0 a 10!";
+	}
+
+	verificaValorDigitado(numeroDigitado);
 
 	if (numeroDigitado == numeroSorteado) {
 		resposta.textContent = "Parabéns você acertou!!!";
 	} else {
 		resposta.textContent = "Errou!! O número sorteado é " + numeroSorteado;
+	}
+}
+
+function verificaValorDigitado() {
+	var campoNumero = document.getElementById("entrada");
+	var numeroDigitado = campoNumero.value;
+
+	if (numeroDigitado < 0 || numeroDigitado > 10) {
+		erro.textContent = "Digite somente números entre 0 a 10!";
+	} else {
+		erro.textContent = "Número válido";
 	}
 }
