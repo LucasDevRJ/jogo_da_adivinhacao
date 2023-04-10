95a6042b2e54ff9b5ff2c2f794040033dea90538
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-09 21:20:50 -0300

    Função para armazenar valor e outra para sortear

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,42 +1,12 @@
 function armazenaValores() {
 	var campoNumero = document.getElementById("entrada");
+	var mensagem = document.getElementById("mensagem");
 	var resposta = document.getElementById("resposta");
 
-	var numeroDigitado = campoNumero.value;
-
-	verificaValorDigitado(numeroDigitado);
+	var numero = campoNumero.value;
 }
 
 function sorteiaNumero() {
-	//Armazenando um número aleatório de 0 a 10
 	var numeroSorteado = Math.random() * 10;
-
-	//Arredondando o número
 	numeroSorteado = Math.round(numeroSorteado);
-
-	verificaAcertoOuErro(numeroSorteado);
-}
-
-function verificaAcertoOuErro(numeroSorteado, numeroDigitado) {
-	if (numeroDigitado == numeroSorteado) {
-		resposta.textContent = "Parabéns você acertou!!!";
-	} else {
-		resposta.textContent = "Errou!! O número sorteado é " + numeroSorteado;
-	}
-}
-
-function verificaValorDigitado(numeroDigitado) {
-	var mensagem = document.getElementById("mensagem")
-
-	console.log(numeroDigitado);
-
-	if (isNaN(numeroDigitado)) {
-		mensagem.textContent = "Digite somente números!";
-	} else if (numeroDigitado.length == 0) {
-		mensagem.textContent = "Digite algum número!";
-	} else if (numeroDigitado < 0 || numeroDigitado > 10) {
-		mensagem.textContent = "Digite somente números de 0 a 10!";
-	} else {
-		mensagem.textContent = "Número válido.";
-	}
 }
