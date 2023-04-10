6d673810e560ee0dcceb67e7d16290c2cd5f83ed
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-09 21:31:42 -0300

    Estrutura de códigos funcionando

--- javaScript/index.js
+++ javaScript/index.js
@@ -3,10 +3,22 @@ function armazenaValores() {
 	var mensagem = document.getElementById("mensagem");
 	var resposta = document.getElementById("resposta");
 
-	var numero = campoNumero.value;
+	var numeroDigitado = campoNumero.value;
+
+	sorteiaNumero(mensagem, resposta, numeroDigitado);
 }
 
-function sorteiaNumero() {
+function sorteiaNumero(mensagem, resposta, numeroDigitado) {
 	var numeroSorteado = Math.random() * 10;
 	numeroSorteado = Math.round(numeroSorteado);
+
+	verificaAcertoOuErro(mensagem, resposta, numeroDigitado, numeroSorteado);
+}
+
+function verificaAcertoOuErro(mensagem, resposta, numeroDigitado, numeroSorteado) {
+	if (numeroDigitado == numeroSorteado) {
+		mensagem.textContent = "Parabéns! Você acertou o número sorteado."
+	} else {
+		mensagem.textContent = "Errou! O número sorteado é " + numeroSorteado;
+	}
 }
